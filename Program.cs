dd0f6baba5595a6759ba2d892c9eb8b06b95d569
Michael Schletz <schletz@gmail.com>
2020-12-14 12:23:17 +0100

    Commit 20201214

--- Program.cs
+++ Program.cs
@@ -21,6 +21,7 @@ namespace Mosviewer
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls("http://0.0.0.0:5000");
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,5 @@
-rd /S /Q Release && dotnet publish -c Release -o Release -p:PublishReadyToRun=true -p:PublishSingleFile=true -r win10-x64
+rd /S /Q Release
+dotnet publish -c Release -o Release -p:PublishReadyToRun=true -p:PublishSingleFile=true -r win10-x64
+dotnet dev-certs https --trust
+del Release.7z
+"C:\Program Files\7-Zip\7z.exe" a -mx=9 Release.7z Release

ec128e1093f69b460799bb1b08e55e3f44849df1
Michael Schletz <schletz@gmail.com>
2021-01-11 18:56:48 +0100

    Commit 20210111

--- Mosviewer.App/Startup.cs
+++ Mosviewer.App/Startup.cs
@@ -35,6 +35,20 @@ namespace Mosviewer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // https://blog.elmah.io/the-asp-net-core-security-headers-guide/
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("X-Frame-Options", "DENY");
+                context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
+                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
+                context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
+                context.Response.Headers.Add("Feature-Policy", "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'");
+                // https://wiki.selfhtml.org/wiki/Sicherheit/Content_Security_Policy
+                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src *");
+                await next();
+            });
+
             app.UseResponseCompression();
 
             if (env.IsDevelopment())
@@ -61,18 +75,7 @@ namespace Mosviewer
                 endpoints.MapRazorPages();
             });
 
-            // https://blog.elmah.io/the-asp-net-core-security-headers-guide/
-            app.Use(async (context, next) =>
-            {
-                context.Response.Headers.Add("X-Frame-Options", "DENY");
-                context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
-                context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                context.Response.Headers.Add("Referrer-Policy", "no-referrer");
-                context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
-                context.Response.Headers.Add("Feature-Policy", "accelerometer 'none'; camera 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; payment 'none'; usb 'none'");
-                context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'");
-                await next();
-            });
+
 
         }
     }

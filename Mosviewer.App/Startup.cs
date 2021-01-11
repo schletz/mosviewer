de3f549d68157c153f6bdecdf03d0d260a0a7ca0
Michael Schletz <schletz@gmail.com>
2021-01-11 19:27:59 +0100

    Commit 20210111

--- Mosviewer.App/Startup.cs
+++ Mosviewer.App/Startup.cs
@@ -39,10 +39,10 @@ namespace Mosviewer
             app.Use(async (context, next) =>
             {
                 context.Response.Headers.Add("X-Frame-Options", "DENY");
-                context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
+                //context.Response.Headers.Add("X-Xss-Protection", "1; mode=block");
                 context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
                 context.Response.Headers.Add("Referrer-Policy", "no-referrer");
-                context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
+                //context.Response.Headers.Add("X-Permitted-Cross-Domain-Policies", "none");
                 context.Response.Headers.Add("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()");                
                 // https://wiki.selfhtml.org/wiki/Sicherheit/Content_Security_Policy
                 context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src *");
